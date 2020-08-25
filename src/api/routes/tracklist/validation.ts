a0834b5c3b9652a1f1e5f2fd78c493dff3024869
zsevic <zsevic95@gmail.com>
2020-08-25 22:13:26 +0200

    Update Joi package

--- src/api/routes/tracklist/validation.ts
+++ src/api/routes/tracklist/validation.ts
@@ -1,5 +1,9 @@
-import Joi from '@hapi/joi';
+import Joi from 'joi';
 
 export const tracklistQuery = Joi.object({
-  url: Joi.string().regex(/^(http(s)??:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/).required(),
+  url: Joi.string()
+    .regex(
+      /^(http(s)??:\/\/)?(www\.)?((youtube\.com\/watch\?v=)|(youtu.be\/))([a-zA-Z0-9\-_])+/,
+    )
+    .required(),
 });
